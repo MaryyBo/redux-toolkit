01c6adff6a50d14762f79ea60c0a50f11c6e9f49
MaryyBo <marusya.bodrug@gmail.com>
2024-07-07 20:45:42 +0200

    Add theme slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slices/counterSlice';
 import langReducer from './slices/languageSlice'
+import themeReducer from './slices/themeSlice'
 
 const store = configureStore({
   reducer: {
     counter: counterReducer, // ключ:значення
-    lang: langReducer
+    lang: langReducer,
+    theme: themeReducer
   }
 });
 
